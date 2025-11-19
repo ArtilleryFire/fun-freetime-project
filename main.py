b86b3dc16eadda955fda3fbb5c81e18e1b6eb414
ArtilleryFire <zidanfnadzirul@gmail.com>
2025-11-20 01:25:50 +0700

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from notify import send_message
+from notify import send_log as send_message
 
 # ==============================
 # UTIL LOGGING
